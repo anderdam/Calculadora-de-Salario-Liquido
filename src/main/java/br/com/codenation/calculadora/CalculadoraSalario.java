81123bb3ba479e8341bd53ca02b23a90d0397147
anderdam <anderdam@gmail.com>
2020-06-26 00:32:44 -0300

    Commit - Fim de projeto

--- src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
+++ src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
@@ -1,21 +1,34 @@
 package br.com.codenation.calculadora;
 
-
 public class CalculadoraSalario {
 
-	public long calcularSalarioLiquido(double salarioBase) {
+	public int calcularSalarioLiquido(double salarioBase) {
 		//Use o Math.round apenas no final do método para arredondar o valor final.
 		//Documentação do método: https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html#round-double-
-		return Math.round(0.0);
-	}
-	
-	
-	//Exemplo de método que pode ser criado para separar melhor as responsábilidades de seu algorítmo
-	private double calcularInss(double salarioBase) {
-		return 0.0;
-	}
 
+		double salarioMenosINSS = 0.0;
+		double salarioMenosIRRF = 0.0;
+
+		if (salarioBase <= 1500.00){
+			salarioMenosINSS = salarioBase - (salarioBase * 0.08);
+		}else if (salarioBase >= 1500.01 && salarioBase <= 4000.00){
+			salarioMenosINSS = salarioBase -  (salarioBase * 0.09);
+		}else if (salarioBase >= 4000.01) {
+			salarioMenosINSS = salarioBase - (salarioBase * 0.11);
+		}
+
+		if (salarioMenosINSS <= 3000.00){
+			salarioMenosIRRF = salarioMenosINSS;
+		}else if (salarioMenosINSS >= 3000.01 && salarioMenosINSS <= 6000.00){
+			salarioMenosIRRF = salarioMenosINSS - (salarioMenosINSS * 0.075);
+		}else if (salarioMenosINSS >= 6000.01){
+			salarioMenosIRRF =  salarioMenosINSS - (salarioMenosINSS * 0.15);
+		}
+
+		if (salarioBase <= 1039.00){
+			return 0;
+		}else {
+			return (int) Math.round(salarioMenosIRRF);
+		}
+	}
 }
-/*Dúvidas ou Problemas?
-Manda e-mail para o meajuda@codenation.dev que iremos te ajudar! 
-*/
